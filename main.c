c3d9f9e2edb8f58ead23de8319fe7534d995e984
AlexGoodlife <novaisboavida@gmail.com>
2023-04-09 23:03:40 +0100

    changed main

--- main.c
+++ main.c
@@ -20,54 +20,21 @@ float lastFrame = 0.0f;
 #define WORLD_HEIGHT 33
 
 // #define SCALING_FACTOR vec3(0.5f, 0.5f, 0.5f)
-//
 
 World *world;
 uint8_t* test_map;
 
 void run() {
 
-  // uint32_t test_map[CHUNK_DEPTH *CHUNK_HEIGHT*CHUNK_WIDTH];
-  test_map = malloc(sizeof(uint8_t) * CHUNK_DEPTH * CHUNK_WIDTH * CHUNK_HEIGHT);
-  for (uint32_t z = 0; z < CHUNK_DEPTH; z++) {
-    for (uint32_t y = 0; y < CHUNK_HEIGHT; y++) {
-      for (uint32_t x = 0; x < CHUNK_WIDTH; x++) {
-        test_map[z * CHUNK_WIDTH * CHUNK_HEIGHT + y * CHUNK_WIDTH + x] = 3;
-      }
-    }
-  }
-
-  test_map[0 + 0 + 0] = 0;
-
-  for (int i = 0; i < CHUNK_HEIGHT; i++) {
-    test_map[CHUNK_DEPTH / 2 * CHUNK_WIDTH * CHUNK_HEIGHT + i * CHUNK_WIDTH +
-             CHUNK_WIDTH / 2] = 0;
-  }
-
-  test_map[CHUNK_DEPTH / 2 * CHUNK_WIDTH * CHUNK_HEIGHT + 3 * CHUNK_WIDTH +
-           CHUNK_WIDTH / 2 - 1] = 0;
-
-  test_map[CHUNK_DEPTH / 2 * CHUNK_WIDTH * CHUNK_HEIGHT +
-           (CHUNK_HEIGHT - 2) * CHUNK_WIDTH + CHUNK_WIDTH / 2] = 1;
-
   GLuint Atlas_texture = loadTexture("resources/big_ass_atlas.png");
 
-  Chunk *map[WORLD_HEIGHT * WORLD_WIDTH];
-  for(int i = 0; i < WORLD_HEIGHT * WORLD_WIDTH;i++){
-    map[i] = chunk_build(test_map);
-  }
-  // Chunk *map[WORLD_HEIGHT * WORLD_WIDTH] = {test, test2, test3, test4};
-
-  // vec3_s offset = vec3_cpy(state->camera->pos);
   vec3_s center = vec3(0,0,0);
 
-   world = world_init(map, WORLD_WIDTH, WORLD_HEIGHT, center);
-
+  world = world_init(WORLD_WIDTH, WORLD_HEIGHT, center);
 
   shader_use(shader);
   shader_setInt(shader, "Atlas", 0);
 
-  // float waitTime = 0.0f;
   mat4_s projection = init_mat4_id;
   projection = linealg_perspective((float)state->windowWidth /(float)state->windowHeight,radians(state->camera->fov), 0.1f, 1000.0f);
 
@@ -97,7 +64,6 @@ void run() {
   }
   world_destroy(world);
   free(test_map);
-  // chunk_destroy(test);
   shader_destroy(shader);
   close();
 }
